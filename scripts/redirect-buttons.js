0d18a0d665a451d6f3b398c722316aeb2a247411
Dmitrii Ch <chdimon@bk.ru>
2024-05-30 15:29:28 +0500

    Practise with gulp

--- scripts/redirect-buttons.js
+++ scripts/redirect-buttons.js
@@ -1,34 +1,34 @@
 function redirectToLogin() {
-    window.location.href = 'login.html';
+  window.location.href = "login.html";
 }
 function redirectToRegistration() {
-    window.location.href = 'registration.html';
+  window.location.href = "registration.html";
 }
 
 function redirectToFrame4() {
-    window.location.href = 'frame-4.html';
+  window.location.href = "frame-4.html";
 }
 
 function redirectToWelcome() {
-    window.location.href = 'welcome.html';
+  window.location.href = "welcome.html";
 }
 
 function redirectToFrame6() {
-    window.location.href = 'frame-6.html';
+  window.location.href = "frame-6.html";
 }
 
 function redirectToFrame11() {
-    window.location.href = 'frame-11.html';
+  window.location.href = "frame-11.html";
 }
 
 function redirectToTest() {
-    window.location.href = 'frame-16.html';
+  window.location.href = "frame-16.html";
 }
 
 function redirectToFrame24() {
-    window.location.href = 'frame-24.html';
+  window.location.href = "frame-24.html";
 }
 
 function redirectToFinish() {
-    window.location.href = 'finish.html';
+  window.location.href = "finish.html";
 }
